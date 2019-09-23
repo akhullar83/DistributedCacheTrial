10abf5a626daed1b921a62101926f75a787bd6e6
Amit Khullar <amit.khullar@nagarro.com>
2019-09-23 11:41:18 +0530

    First commit for session management using distributed cache in .net core

---
+++ DistributedCacheTrial/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace DistributedCacheTrial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+
+            _distributedCache = distributedCache;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+
+            HttpContext.Session.SetString(HttpContext.Session.Id, "User:"+ HttpContext.Session.Id);
+            
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+
+            string sessid = HttpContext.Session.Id;
+
+            return HttpContext.Session.GetString(HttpContext.Session.Id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DistributedCacheTrial/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace DistributedCacheTrial
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            ConfigurationOptions config = new ConfigurationOptions
+            {
+                EndPoints =
+                {
+                    { "35.232.241.220", 6379 },
+                },
+                
+                KeepAlive = 180,
+            };
+
+            services.AddDistributedRedisCache(o =>
+            {
+                o.ConfigurationOptions = config;
+            });
+
+            services.AddSession(options => {
+                options.CookieName = "Test.Session";
+                options.IdleTimeout = TimeSpan.FromSeconds(3600);
+            });
+
+            services.AddMvc();
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseSession();
+            app.UseMvc();
+            //app.Run(context => {
+            //    return context.Response.Headers.Response.WriteAsync("Hello Readers!");
+            //});
+        }
+    }
+}
